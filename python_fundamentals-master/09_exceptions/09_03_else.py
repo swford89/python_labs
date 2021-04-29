43d05cbe9e93399f67417ef5132cb19d972a6103
Scott Ford <swford@ualberta.ca>
2021-04-29 09:21:17 -0600

    completed 09_03_else

--- python_fundamentals-master/09_exceptions/09_03_else.py
+++ python_fundamentals-master/09_exceptions/09_03_else.py
@@ -2,3 +2,24 @@
 Write a script that demonstrates a try/except/else.
 
 '''
+while True:
+
+    try:
+        numerator = int(input("Enter a whole number: "))
+        denominator = int(input("Enter a whole number to divide by: "))
+        print(f"The quotient of {numerator} divided by {denominator} is: {numerator/denominator}")
+
+    except ValueError:
+        print("""
+        Be sure to enter a number and not a letter.
+        """)
+
+    except ZeroDivisionError:
+        print("""
+        You're not allowed to divide by zero.
+        """)
+        
+    else:
+        print("""
+        No errors encountered.
+        """)

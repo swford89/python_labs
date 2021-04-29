91ea8dc4b3137e9d4ba7aa945f2f4c47ad1ffeef
Scott Ford <swford@ualberta.ca>
2021-04-29 09:14:05 -0600

    completed 09_01_first_exception

--- python_fundamentals-master/09_exceptions/09_01_first_exception.py
+++ python_fundamentals-master/09_exceptions/09_01_first_exception.py
@@ -8,3 +8,9 @@ print(list_[1])
 This raises and exception that needs to be handled.
 
 '''
+try:
+    list_ = ["hello world!"]
+    print(list_[1])
+
+except IndexError:
+    print("You don't have an item at that index.")

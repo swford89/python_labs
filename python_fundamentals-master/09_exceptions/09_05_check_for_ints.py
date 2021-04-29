224a862a44cb1c28bc35c345f097bac252ff116d
Scott Ford <swford@ualberta.ca>
2021-04-29 10:46:22 -0600

    completed 09_05_check_for_ints

--- python_fundamentals-master/09_exceptions/09_05_check_for_ints.py
+++ python_fundamentals-master/09_exceptions/09_05_check_for_ints.py
@@ -6,3 +6,15 @@ the input was an integer or not.
 The script should keep prompting the user until they enter an integer.
 
 '''
+
+while True:
+
+    try:
+        integer_prompt = int(input("Enter any integer: "))
+        if type(integer_prompt) == int:
+            break
+
+    except ValueError:
+        print("""
+        What you entered doesn't seem to be an integer. Try again.
+        """)

ce2d99d606ed6e5d9604a2ece47efd899c92d807
Scott Ford <swford@ualberta.ca>
2021-06-01 12:54:35 -0600

    completed 13_05_shirt_shop

--- python_fundamentals-master/13_list_comprehensions/13_05_shirt_shop.py
+++ python_fundamentals-master/13_list_comprehensions/13_05_shirt_shop.py
@@ -8,3 +8,14 @@ Then open up your online shop ;)
 
 colors = ["neon orange", "spring green"]
 sizes = ["S", "M", "L"]
+
+new_list = []
+
+for color in colors:
+    for size in sizes:
+        color_size = f'{color}: {size}'
+        new_list.append(color_size)
+print(new_list)
+
+colour_size = [f'{color}: {size}' for color in colors for size in sizes]
+print(colour_size)

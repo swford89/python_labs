aa0e2014070327722813890938e0c424b0a20fe8
Scott Ford <swford@ualberta.ca>
2021-07-09 09:56:58 -0600

    completed 04_07_search.py

--- python_fundamentals-master/04_conditionals_loops/04_07_search.py
+++ python_fundamentals-master/04_conditionals_loops/04_07_search.py
@@ -4,3 +4,20 @@ Receive a number between 0 and 1,000,000,000 from the user.
 Use while loop to find the number - when the number is found exit the loop and print the number to the console.
 
 '''
+while True:
+    try:
+        user_num = int(input('Enter a number between 0 and 1,000,000,000: '))
+        break
+    except ValueError:
+        print("""
+        You're only allowed to enter an integer.
+        """)
+
+counter = 0
+while counter < 1000000000:
+    if counter == user_num:
+        print(f"Found the number you entered: {counter}")
+        break
+    else:
+        print(counter)
+        counter += 1

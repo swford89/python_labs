f4e31fad001419f6acd422a211cfafef95af1687
Scott Ford <swford@ualberta.ca>
2021-07-14 15:33:52 -0600

    completed 04_10_squares.py

--- python_fundamentals-master/04_conditionals_loops/04_10_squares.py
+++ python_fundamentals-master/04_conditionals_loops/04_10_squares.py
@@ -4,4 +4,5 @@ Write a script that prints out all the squares of numbers from 1- 50
 Use a for loop that demonstrates the use of the range function.
 
 '''
-
+for num in range(1,51):
+    print(num**2)
